c83da57e79f3f766289b8f04704e5c23d0efbe42
Gustavo Henrique <igumartins.2001@gmail.com>
2022-09-09 01:18:58 -0300

    js accordion

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// CONTEUDO DA IMAGEM CLICADA (tabNavegation)
+
+function initTabNav() {
+  const fotoAnimais = document.querySelectorAll(".js-tabmenu img");
+  const textoAnimais = document.querySelectorAll(".js-tabcontent section");
+  const activeClass = "ativo";
+
+  if (fotoAnimais.length && textoAnimais.length) {
+    textoAnimais[0].classList.add(activeClass);
+
+    function clickAnimal(index) {
+      textoAnimais.forEach((texto) => {
+        texto.classList.remove(activeClass);
+      })
+      textoAnimais[index].classList.add(activeClass);
+    }
+
+    fotoAnimais.forEach((foto,index) => {
+      foto.addEventListener("click", () => {
+        clickAnimal(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+// FAQ RESPOSTAS (Accordion List)
+
+
+function initAccordion() {
+  const perguntas = document.querySelectorAll(".js-accordion dt")
+  const activeClass = "ativo";
+
+  if (perguntas.length) {
+    perguntas[0].classList.add(activeClass)
+    perguntas[0].nextElementSibling.classList.add(activeClass)
+
+    function activeAccordion () {
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
+    }
+
+    perguntas.forEach((pergunta) => {
+      pergunta.addEventListener("click",activeAccordion)
+    })
+  }
+}
+initAccordion();
+
+// SCROLL SUAVE 
+
+const linksInternos = document.querySelectorAll(".js-menu a[href^='#']")
+console.log(linksInternos)
+
+
+
+
+  
+
