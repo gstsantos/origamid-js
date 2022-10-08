2fbecb5fd9dcae3b1a9b4ff42255c26fb1319e67
Gustavo Henrique <igumartins.2001@gmail.com>
2022-10-08 14:41:00 -0300

    modal refatorado

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
+import Modal from './modules/modal.js';
 import initToolTip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
@@ -10,16 +10,22 @@ import iniFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimationScroll from './modules/scroll-animacao.js';
 
+// links internos, options
 const scrollSuave = new ScrollSuave("[data-menu='suave'] a[href^='#']");
 scrollSuave.init();
 
+// lista do accordion, classe
 const accordion = new Accordion("[data-anime='accordion'] dt", 'ativo');
 accordion.init();
 
+// parâmetros = menu, conteudo
 const tabNav = new TabNav("[data-tab='menu'] li", "[data-tab='content'] section");
 tabNav.init();
 
-initModal();
+// parâmetros = botao de abrir o modal, botao de fechar o modal e o container do modal
+const modal = new Modal("[data-modal='abrir']", "[data-modal='fechar']", "[data-modal='container']");
+modal.init();
+
 initToolTip();
 initDropDownMenu();
 initMenuMobile();
