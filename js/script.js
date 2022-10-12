babc8600f9339d5adb692fe19c8f067dc1162e79
Gustavo Henrique <igumartins.2001@gmail.com>
2022-10-11 21:36:57 -0300

    menu-mobile refatorado

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
+import MenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
@@ -33,7 +33,9 @@ tooltip.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]', ['touchstart', 'click'], 'active');
 dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']", ['click', 'touchstart'], 'active');
+menuMobile.init();
+
 initFuncionamento();
 // argumentos = url da api, e o alvo onde será adicionado os items da api
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
